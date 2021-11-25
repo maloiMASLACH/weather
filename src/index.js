8248004ba5d346128dfaf9e411b6498a4cc26ed2
maloiMASLACH <60549661+maloiMASLACH@users.noreply.github.com>
2021-11-25 11:11:23 +0300

    feat: clear mess

--- src/index.js
+++ src/index.js
@@ -2,18 +2,4 @@ import App from "./app";
 
 const app = new App();
 app.run();
-/* import './style.css';
 
-class GetInfo {
-  getWhatINeed() {
-    return fetch(
-      'https://api.weatherapi.com/v1/forecast.json?key=01960bba4b6a444a831133935212311&q=Minsk&days=1&aqi=no&alerts=no',
-    ).then((res) => { this.allInfo = res; return res.json(); });
-  }
-}
-
-const showAll = async () => {
-  const list = await new GetInfo().getWhatINeed();
-  console.log(list);
-};
-showAll(); */
--- src/menu/menu.js
+++ src/menu/menu.js
@@ -1,12 +1,7 @@
 import MenuTemplate from "../templates/menuTemplate";
 import PagesIds from "../pages/pageIDs";
 import "./menu.css";
-/*const ButtonsNames = [
-  { id: Object.keys(PagesIds)[0], img: "" },
-  { id: Object.keys(PagesIds)[1], img: "" },
-  { id: Object.keys(PagesIds)[2], img: "" },
-];
-*/
+
 export class Menu extends MenuTemplate {
   constructor(tagName, className) {
     super(tagName, className);
@@ -21,15 +16,6 @@ export class Menu extends MenuTemplate {
       buttons.append(btnHTML);
       console.log(btnHTML);
     });
-
-    /*  ButtonsNames.forEach((btn)=>{
-        const btnHTML=document.createElement('a')
-        btnHTML.href=`#${btn.id}`
-        btnHTML.text=`${btn.id}`
-        buttons.append(btnHTML)
-        console.log(btnHTML)
-    })*/
-
     this.conteiner.append(buttons);
   }
   render() {
