c1da19b5ced82ea1143b0fc3fa9c24c479f13eb8
maloiMASLACH <60549661+maloiMASLACH@users.noreply.github.com>
2021-11-29 09:08:34 +0300

    docs: add icons for light theme

---
+++ src/data/icons.js
@@ -0,0 +1,22 @@
+const icons = {
+  'Light rain': 'rain_light',
+  'Light rain shower': 'snow',
+  'Light sleet': 'snow',
+  'Light snow': 'snow',
+  'Heavy snow':'snow',
+  'Moderate or heavy sleet': 'snow',
+  'Light drizzle': 'rain_light',
+  'Moderate rain': 'rain_huge',
+  'Partly cloudy': 'sun_cloud',
+  Clear: 'sunny',
+  Sunny: 'sunny',
+  Overcast: 'cloud',
+  Fog: 'cloud',
+  "Cloudy":"cloud",
+  "Mist":'cloud',
+  'Patchy rain possible':'rain_light',
+  'Moderate snow':'snow',
+  'Blowing snow':'snow',
+  'Blizzard':'snow'
+};
+export default icons;
