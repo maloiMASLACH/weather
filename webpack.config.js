13a7a8dc201990858d3375b22ba9a874dceb0b33
maloiMASLACH <60549661+maloiMASLACH@users.noreply.github.com>
2021-11-29 15:12:16 +0300

    docs: rebuild progect

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require("path");
 const HtmlWebpackPlugin=require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const {CleanWebpackPlugin}=require('clean-webpack-plugin')
 
 const devServer = (isDev) => !isDev ? {} : {
     devServer: {
       open: true,
-      port: 3000
+      port: 3000,
     },
   };
 
@@ -17,6 +18,7 @@ module.exports =({develop})=> ({
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
+    assetModuleFilename: 'fonts/[hash][ext]',
   },
   module: {
     rules: [
@@ -47,6 +49,11 @@ module.exports =({develop})=> ({
       }),
     new HtmlWebpackPlugin({
         template:'./src/index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'public' },
+      ],
     }),
     new CleanWebpackPlugin()
   ],
