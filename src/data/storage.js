54aaf95b47b6a5730c589e7abfa1b71fcece2464
maloiMASLACH <60549661+maloiMASLACH@users.noreply.github.com>
2021-12-01 11:44:16 +0300

    feat: add sity API

--- src/data/storage.js
+++ src/data/storage.js
@@ -3,16 +3,30 @@ class GetInfo {
     return fetch(
       'https://api.weatherapi.com/v1/forecast.json?key=01960bba4b6a444a831133935212311&q=Minsk&days=3&aqi=yes&alerts=yes',
     ).then((res) => {
-      this.allInfo = res;
       return res.json();
     });
   }
 
+
+  sityAPI() {
+  return fetch(
+    `https://autocomplete.travelpayouts.com/places2?term=Mins&locale=en&types[]=city`
+  ).then((res)=>{
+    return res.json()
+  })
+  
+    
+  }
+
   async showAll() {
     const list = await this.getWhatINeed();
-
     return list;
   }
-}
+  async sityes() {
+    const list = await this.sityAPI();
+    console.log(list)
+    return list;
+  }
 
+}
 export default GetInfo;
